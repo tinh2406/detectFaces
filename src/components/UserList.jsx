499a5a87dc5097fb5ff1d9185dae790dd02a2d23
tinh2406 <nguyenquoctinh24062002@gmail.com>
2023-04-22 14:20:11 +0700

    Update get UserList

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,7 +1,9 @@
 import {API_URL} from '@env';
-import {useContext, useState, useEffect} from 'react';
+import firestore from "@react-native-firebase/firestore"
+import React, {useContext, useState, useEffect} from 'react';
 import axios from 'axios';
 import {AuthContext} from '../contexts/authContext';
+import {useNetInfo} from '@react-native-community/netinfo';
 import {
   ActivityIndicator,
   Alert,
@@ -15,20 +17,58 @@ import {
   TouchableOpacity,
   SafeAreaView,
 } from 'react-native';
-
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import deepEqual from "deep-equal"
 export default function UserList({setUserListPanel}) {
-  const [userList, setUserList] = useState('');
+  const [userList, setUserList] = useState([]);
   const {user} = useContext(AuthContext);
+  const netInfor = useNetInfo();
 
-  useEffect(() => {
-    axios
-      .get(`${API_URL}:3000/users/userList`)
-      .then(res => {
-        const filteredData = res.data.filter(item => item.owner == user.phone);
-        setUserList(filteredData);
+  useFocusEffect(
+    React.useCallback(()=>{
+      const getUserListLocal = async()=>{
+        if(!userList){
+          setUserList(JSON.parse(await AsyncStorage.getItem('userList')))
+        }
+      }
+      getUserListLocal()
+      return()=>{
+        getUserListLocal
+      }
+    },[userList])
+  )
+  useFocusEffect(
+    React.useCallback(() => {
+
+      if(user.owner && netInfor.isConnected){
+        console.log("Đã chạy tới đây")
+        
+      const userRef = firestore().collection("users").where("owner","==",user.phone)
+      const unsubscribe = userRef.onSnapshot(async snapshot=>{
+        const users = []
+        await Promise.all(
+          snapshot.docs.map(async doc=>{
+            const name = await doc.data().name
+            const phone = await doc.data().phone
+            if(doc.exists){
+              users.push({name,phone})
+            }
+          })
+        );
+
+        if(userList?.length!==users.length || !deepEqual(userList,users)){
+          setUserList(users)
+          await AsyncStorage.setItem("userList",JSON.stringify(users))
+          console.log("Reset userList")
+        }
       })
-      .catch(error => console.error(error));
-  }, []);
+      return ()=>{
+        unsubscribe();
+      }
+    }
+  }, [netInfor,user])
+  )
 
   return (
     <SafeAreaView>
